d77a64ce4cbf1f2fafd8089dcb5ba92373a6db53
Pere Gil <pgil@ieec.uab.es>
2017-09-18 03:43:07 +0200

    phd2 basic client developed

--- obscontrol/phd2_client.py
+++ obscontrol/phd2_client.py
@@ -10,10 +10,43 @@ import time
 
 HOST, PORT = "localhost", 4400
 
-#~ m ='{"method": "guide", "params": [{"pixels": 1.5, "time": 8, "timeout": 40}, false], "id": 1}'
-m ='{"method":"set_paused","params":[true,"full"],"id":1}'
+#Guide with Settle parameters (1.5,8,40) | No recalibration (false))
+#~ m ='{"method": "guide", "params": [{"pixels": 1.5, "time": 5, "timeout": 40}, false], "id": 1}'
+
+#Guide with Settle parameters (1.5,8,40) | Recalibration (true)
+#~ m ='{"method": "guide", "params": [{"pixels": 1.5, "time": 5, "timeout": 40}, true], "id": 1}'
+
+#Start capturing, or, if guiding, stop guiding but continue capturing
+#~ m = '{"method":"loop", "id":1}'
+
+#Stop capturing and also stop guiding
+#~ m  = '{"method":"stop_capture", "id":1}'
+
+#Set pause ON
+#~ m ='{"method":"set_paused","params":[true,"full"],"id":1}'
+
+#Connect (true)  all equipment
+#~ m = '{"method":"set_connected", "params":[true], "id":1}'
+
+#Disconnect (false) all equipment
+#~ m = '{"method":"set_connected", "params":[false], "id":1}'
+
+#Set pause OFF
+#~ m ='{"method":"set_paused","params":[false,"full"],"id":1}'
+
+#Find a start
 #~ m ='{"method":"find_star","id":1}'
-#~ m ='{"method":"get_app_state","id":1}'
+
+#Get exposure time
+#~ m ='{"method":"get_exposure","id":1}'
+
+#Set exposure time
+#~ m = '{"method": "set_exposure", "params": [500], "id": 1}'
+
+#Get App state
+m ='{"method":"get_app_state","id":1}'
+
+#Dither with Settle parameters
 #~ m = '{"method": "dither", "params": [2, false, {"pixels": 1.5, "time": 8, "timeout": 40}], "id": 1}'
 
 
@@ -26,7 +59,7 @@ try:
     # Receive data from the server and shut down
     sock.send(m+'\n')
     time.sleep(1)
-    received = sock.recv(512)
+    received = sock.recv(1024)
     
 finally:
     sock.close()
